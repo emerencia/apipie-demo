2309a7857ace417f65f615fb7b6783d104a9909e
Ando Emerencia <a.c.emerencia@researchable.nl>
2023-03-24 16:21:22 +0100

    feat(generate): generate some stff

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Demo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.api_only = true
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -48,10 +49,10 @@ module Demo
     # config.active_record.schema_format = :sql
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    #config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    #config.assets.version = '1.0'
 
     config.active_support.test_order = :random
   end
